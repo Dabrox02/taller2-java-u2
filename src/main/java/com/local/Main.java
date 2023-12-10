d5b780efb5ef17bc85c83d13bc5b84e77a86e68d
Jaider Mendoza <jaiderstyvenmendoza@gmail.com>
2023-12-10 07:24:14 -0500

    feat: :sparkles: romano to int done

--- src/main/java/com/local/Main.java
+++ src/main/java/com/local/Main.java
@@ -1,9 +1,42 @@
 package com.local;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println(cifradoCesar("holaZ_*&", 1));
+        System.out.println(romanoAEntero("IV"));
+    
     }
+
+    static int romanoAEntero(String romano){
+        Map<Character, Integer> letrasRomanas = new HashMap<>();
+        letrasRomanas.put('I', 1);
+        letrasRomanas.put('V', 5);
+        letrasRomanas.put('X', 10);
+        letrasRomanas.put('L', 50);
+        letrasRomanas.put('C', 100);
+        letrasRomanas.put('D', 500);
+        letrasRomanas.put('M', 1000);
+
+        romano = romano.toUpperCase();
+        char[] letras = romano.toCharArray();
+        int entero = 0;
+    
+        for (int i = 0; i < letras.length; i++) {
+            int valorActual = letrasRomanas.get(letras[i]);
+    
+            if (i < letras.length - 1 && letrasRomanas.get(letras[i + 1]) > valorActual) {
+                entero += letrasRomanas.get(letras[i + 1]) - valorActual;
+                i++;
+            } else {
+                entero += valorActual;
+            }
+        }
+        return entero;
+    }
+
 
     static String cifradoCesar(String cadena, int desplazamiento) {
         char[] letras = cadena.toCharArray();
